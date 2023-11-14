6228424781b5a19d353546a3e5e380558409acf0
younusaliakash <younusalikash8@gmail.com>
2023-11-14 19:38:51 +0600

    UI design done

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <script setup>
-
+import Header from "./components/Header.vue";
+import Balance from "./components/Balance.vue";
+import IncomeExpense from "./components/IncomeExpensesBoard.vue";
+import TransactionHistory from "./components/TransactionHistory.vue";
+import AddTransaction from "./components/AddTransaction.vue";
 </script>
 
 <template>
-  <h1>Hello from app</h1>
+  <Header />
+  <div class="container">
+    <Balance/>
+    <IncomeExpense/>
+    <TransactionHistory/>
+    <AddTransaction/>
+  </div>
 </template>
-
-<style scoped>
-
-</style>
---
+++ src/components/Balance.vue
@@ -0,0 +1,4 @@
+<template>
+    <h4>Your Balance</h4>
+    <h1 id="balance">$100</h1>
+  </template>
